be53d137400a15f3c8af0e59e517874842157980
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-23 18:08:01 +0000

    register resolver

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import "reflect-metadata";
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
 import { createConnection } from 'typeorm';
+
 import { resolvers } from "./resolvers/resolver"
 
 const typeDefs = importSchema(`${__dirname}/typedefs.graphql`);
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-createConnection().then(()=> console.log("connected to DB"));
-server.start(() => console.log('Server is running on localhost:4000'));
+// start server after connected to PSQL
+createConnection().then(()=> {
+    console.log("Connected to DB");
+    server.start(() => console.log('Server running on localhost:4000'));
+});
--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,10 +1,22 @@
+import * as bcrypt from "bcryptjs";
+
 import { IResolver } from "./types/resolvertypes";
+import { User } from "../entity/User";
 
 export const resolvers: IResolver = {
     Query: {
       hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || 'World'}`,
     },
     Mutation: {
-      register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {email + password},
+      register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const userCreated = User.create({
+          email,
+          password: hashedPassword
+        });
+        // result of saved User object once stored in the DB
+        const res: User = await userCreated.save();
+        return `user created ${res.id} ${res.email}`;
+      },
     }
   }
