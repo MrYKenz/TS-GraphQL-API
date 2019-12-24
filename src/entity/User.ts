74f21046253b81fe21595e0253d8335ba6deecc0
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-24 22:24:06 +0000

    test output using jest describe and verbose flag

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,9 +1,10 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+import {Entity, PrimaryColumn, Column, BaseEntity, BeforeInsert} from "typeorm";
+import * as uuidv4 from "uuid/v4";
 
 @Entity("users") // table name users to not conflict with postgres users table that already exists
 export class User extends BaseEntity {
     // uuid to not expose number of records (security)
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryColumn("uuid")
     id: string;
 
     @Column("varchar", { length: 255, unique: true })
@@ -11,4 +12,9 @@ export class User extends BaseEntity {
 
     @Column("text") // any lenght for hashed pswrd
     password: string;
+
+    @BeforeInsert()
+    addId() {
+        this.id = uuidv4();
+    }
 }
--- tests/register.test.ts
+++ tests/register.test.ts
@@ -9,15 +9,17 @@ const registerMutation: string = `mutation {
     register(email: "${email}" password: "${password}")
 }`
 
-test("register user", async () => {
+describe("register user", () => {
     // use graphql-request to run GQL against endpoint
-    const res = await request("http://localhost:4000", registerMutation);
-    // check register object with string is returned
-    expect(res).toEqual({ register: expect.any(String) });
-    // search DB for user & check only 1 exists 
-    await createConnection();
-    const users = await User.find({ where: { email } });
-    expect(users).toHaveLength(1);
-    // check password is hashed - not the same as plain string
-    expect(users[0].password).not.toEqual(password);
+    test("register user function works", async () => {
+        const res = await request("http://localhost:4000", registerMutation);
+        expect(res).toEqual({ register: expect.any(String) });
+    });
+
+    test("no duplicate users & hashed pw", async () => {
+        await createConnection();
+        const users = await User.find({ where: { email } });
+        expect(users).toHaveLength(1);
+        expect(users[0].password).not.toEqual(password);
+    });
 });
